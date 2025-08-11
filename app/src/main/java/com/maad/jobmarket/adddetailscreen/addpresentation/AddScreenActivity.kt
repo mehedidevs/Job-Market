6ddad75332a912dae85407f0a7c95ca80602c8c8
MD. Jubayer Ahmed <63640474+jubayerappdev@users.noreply.github.com>
2025-08-12 01:57:43 +0600

    initial some resources added

--- app/src/main/java/com/maad/jobmarket/adddetailscreen/addpresentation/AddScreenActivity.kt
+++ app/src/main/java/com/maad/jobmarket/adddetailscreen/addpresentation/AddScreenActivity.kt
@@ -1,10 +1,7 @@
-package com.maad.jobmarket.presentation.adddetailscreen
+package com.maad.jobmarket.adddetailscreen.addpresentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.maad.jobmarket.R
 import dagger.hilt.android.AndroidEntryPoint
 
